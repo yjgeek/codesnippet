05634262ba7db4346e9e28ca62c15b006282544a
zfowed <zfowed@gmail.com>
2017-07-27 15:04:26 +0800

    Update fileSelectionClass.js

--- fileSelectionClass.js
+++ fileSelectionClass.js
@@ -15,23 +15,24 @@
  *         // cutFileDatas: 当前选择的解析过的文件数据
  *         // fileDatas: 所以已经选择的解析过的文件数据
  *     },
- *     onError: function (type, errFileData, cutFileDatas, fileDatas) { // 监听选择文件出错事件
+ *     onError: function (errType, errFileData, cutFileDatas, fileDatas) { // 监听选择文件出错事件
  *         // errType: 错误类型，支持 limitExt, maxSize, minSize
  *         // errFileData: 出错的文件数据
  *         // cutFileDatas: 当前选择的解析过的文件数据
  *         // fileDatas: 所以已经选择的解析过的文件数据
  *     }
  * });
- * 
+ *
+ * fileSelection.$values     // 获取当前所有选择的文件数据
  * fileSelection.select()    // 打开选择文件窗口
  * fileSelection.push()      // 打开添加文件窗口
- * fileSelection.files()     // 获取当前所有选择的文件
+ * fileSelection.files()     // 获取当前所有选择的文件对象
  * fileSelection.remove(id)  // 根据fileData中id删除已经选择的文件
  * 
  */
-var FileSelection = (function () {
+var FileSelection = (function() {
 
-	function FileSelection (opt) {
+	function FileSelection(opt) {
 		// 多选文件
 		this.$multiple = !!opt.multiple;
 		// 限制选择的文件后缀类型
@@ -60,7 +61,7 @@ var FileSelection = (function () {
 	}
 
 	/** 创建存放input标签的容器 */
-	FileSelection.prototype.$$createInputFileBox = function () {
+	FileSelection.prototype.$$createInputFileBox = function() {
 		if (!this.$$eInputFileBox) {
 			FileSelection.prototype.$$eInputFileBox = document.createElement('div');
 			document.body.appendChild(FileSelection.prototype.$$eInputFileBox);
@@ -68,7 +69,7 @@ var FileSelection = (function () {
 	};
 
 	/** 创建input标签 */
-	FileSelection.prototype.$$createInputFile = function () {
+	FileSelection.prototype.$$createInputFile = function() {
 		if (this.$eInputFile && this.$$eInputFileBox) {
 			this.$$eInputFileBox.removeChild(this.$eInputFile);
 		}
@@ -77,37 +78,38 @@ var FileSelection = (function () {
 		this.$eInputFile.style.display = 'none';
 		this.$eInputFile.onchange = this.$$onChange.bind(this);
 
-		if ( this.$multiple ) { this.$eInputFile.multiple = "multiple"; }
-		if ( this.$suffix ) { this.$eInputFile.accept = this.$suffix; }
-		
+		if (this.$multiple) { this.$eInputFile.multiple = "multiple"; }
+		if (this.$suffix) { this.$eInputFile.accept = this.$suffix; }
+
 		this.$$eInputFileBox.appendChild(this.$eInputFile);
 	};
 
 	/** 获取一个随机的 ID */
-	FileSelection.prototype.$$getId = function () {
+	FileSelection.prototype.$$getId = function() {
 		var str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 		var result = ''
-		for (var i = 0; i < 32; i++) {
+		for (var i = 0; i < 8; i++) {
 			result += str[Math.floor(Math.random() * str.length)];
 		}
 		return result;
 	};
 
 	/** 根据image file对象获取base64值 */
-	FileSelection.prototype.$$parseImgBase64 = function (file, callback) {
+	FileSelection.prototype.$$parseImgBase64 = function(file, callback) {
 		if (typeof FileReader !== 'function') { return callback && callback(''); }
 		var reader = new FileReader();
-		reader.onload = function () {
+		reader.onload = function() {
 			callback && callback(this.result);
 		}
 		reader.readAsDataURL(file);
 	};
 
 	/** 根据file对象生成fileValue */
-	FileSelection.prototype.$$parseFileData = function (file) {
+	FileSelection.prototype.$$parseFileData = function(file) {
 		var fileData = {};
 		fileData.id = this.$$getId();
-		file.name.replace(/^(((?:[a-zA-Z]):)?[\\\/]?(?:[\s\S]+?[\\\/])*)([\s\S]+?(\.[\s\S]+?)?)$/, function (all, dirname, drive, basename, extname) {
+		fileData.path = file.name;
+		fileData.path.replace(/^(((?:[a-zA-Z]):)?[\\\/]?(?:[\s\S]+?[\\\/])*)([\s\S]+?(\.[\s\S]+?)?)$/, function(all, dirname, drive, basename, extname) {
 			fileData.drive = drive || undefined;
 			fileData.dirname = dirname || undefined;
 			fileData.basename = basename || undefined;
@@ -122,11 +124,11 @@ var FileSelection = (function () {
 	};
 
 	/** 任务队列 */
-	FileSelection.prototype.$$queue = function (tasks, callback, success) {
+	FileSelection.prototype.$$queue = function(tasks, callback, success) {
 		var tasksLen = tasks.length;
 		var successNum = 0;
-		var next = function () {
-			successNum +=1;
+		var next = function() {
+			successNum += 1;
 			if (successNum >= tasksLen) {
 				success && success();
 			}
@@ -137,28 +139,28 @@ var FileSelection = (function () {
 	};
 
 	/** input选择更改事件 */
-	FileSelection.prototype.$$onChange = function () {
+	FileSelection.prototype.$$onChange = function() {
 		var self = this;
 		var files = self.$eInputFile.files;
 		var cutFileDatas = [];
-		self.$$queue(files, function (file, next) {
+		self.$$queue(files, function(file, next) {
 			var fileData = self.$$parseFileData(file);
 			cutFileDatas.push(fileData);
-			if (!self.$parseImgBase64 || ['.jpg','.png','.gif'].indexOf(fileData.extname) < 0) {
+			if (!self.$parseImgBase64 || ['.jpg', '.png', '.gif'].indexOf(fileData.extname) < 0) {
 				return next();
 			}
-			self.$$parseImgBase64(file, function (result) {
+			self.$$parseImgBase64(file, function(result) {
 				fileData.result = result
 				return next();
 			});
-		}, function () {
+		}, function() {
 			var err = null;
-			cutFileDatas.forEach(function (fileData) {
-				if ( self.$maxSize && fileData.size > self.$maxSize ) {
+			cutFileDatas.forEach(function(fileData) {
+				if (self.$maxSize && fileData.size > self.$maxSize) {
 					return err = { type: 'maxSize', fileData: fileData };
-				} else if ( self.$minSize && fileData.size < self.$minSize ) {
+				} else if (self.$minSize && fileData.size < self.$minSize) {
 					return err = { type: 'minSize', fileData: fileData };
-				} else if ( self.$limitExt && self.$limitExt.indexOf(fileData.extname) < 0 ) {
+				} else if (self.$limitExt && self.$limitExt.indexOf(fileData.extname) < 0) {
 					return err = { type: 'limitExt', fileData: fileData };
 				}
 			});
@@ -171,8 +173,8 @@ var FileSelection = (function () {
 	};
 
 	/** 注册事件 */
-	FileSelection.prototype.on = function (eType, callback) {
-		if (typeof callback !== 'function') { return ; }
+	FileSelection.prototype.on = function(eType, callback) {
+		if (typeof callback !== 'function') { return; }
 		if (eType === 'change') {
 			this.$onChange = callback;
 		} else if (eType === 'error') {
@@ -181,30 +183,30 @@ var FileSelection = (function () {
 	};
 
 	/** 打开选择文件窗口 */
-	FileSelection.prototype.select = function () {
+	FileSelection.prototype.select = function() {
 		this.$action = 'select';
 		this.$eInputFile.click();
 	};
 
 	/** 打开添加文件窗口 */
-	FileSelection.prototype.push = function () {
+	FileSelection.prototype.push = function() {
 		this.$action = 'push';
 		this.$eInputFile.click();
 	};
 
 	/** 获取所有已选择的file对象 */
-	FileSelection.prototype.files = function () {
-		return this.$values.map(function (item) {
+	FileSelection.prototype.files = function() {
+		return this.$values.map(function(item) {
 			return item.$file;
 		});
 	};
 
 	/** 根据id删除fileValue */
-	FileSelection.prototype.remove = function (id) {
-		var fileIndex = this.$values.findIndex(function (item) {
+	FileSelection.prototype.remove = function(id) {
+		var fileIndex = this.$values.findIndex(function(item) {
 			return item.id === id;
 		});
-		if (fileIndex < 0) { return ; }
+		if (fileIndex < 0) { return; }
 		this.$values.splice(fileIndex, 1);
 	};
 
